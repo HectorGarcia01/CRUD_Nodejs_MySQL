e8a3a9597a87c962076058fd3f94e41d0250d07d
HectorGarcia01 <hg2779948@gmail.com>
2023-07-20 02:29:44 -0600

    Creación de la propiedad para obtener la información de un cliente por su id

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -38,6 +38,28 @@ controller.add = (req, res) => {
     });
 }
 
+//Definimos una propiedad para obtener los datos de un cliente
+controller.read = (req, res) => {
+    //Obtenemos el id del cliente
+    const { id } = req.params;
+
+    //Pedimos una conexión a mysql
+    req.getConnection((err, conn) => {
+        conn.query('SELECT *FROM customer WHERE id = ?', [id], (err, customer) => {
+            //En caso de obtener un error
+            if (err) {
+                //Enviamos un estado 400 con el error
+                res.status(400).send({ error: "Error al borrar el cliente.", err });
+            }
+
+            //Renderizamos la vista customer_edit.ejs
+            res.render('customer_edit', {
+                customer: customer[0]
+            });
+        });
+    });
+}
+
 
 //Exportamos el objeto
 module.exports = controller;
