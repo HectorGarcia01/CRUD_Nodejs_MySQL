eec3d8566184fd74e034658378b687dec69915ac
HectorGarcia01 <hg2779948@gmail.com>
2023-07-20 02:28:23 -0600

    Creación de la propiedad para crear un nuevo cliente

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -18,5 +18,26 @@ controller.list = (req, res) => {
     });
 };
 
+//Definimos una propiedad para crear nuevo cliente
+controller.add = (req, res) => {
+    //Obtenemos los datos del formulario
+    const data = req.body;
+
+    //Pedimos una conexión a mysql
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO customer set ?', [data], (err, customer) => {
+            //En caso de obtener un error
+            if (err) {
+                //Enviamos un estado 400 con el error
+                res.status(400).send({ error: "Error al crear el cliente.", err });
+            }
+
+            //Redireccionamos a la ruta principal
+            res.redirect('/');
+        });
+    });
+}
+
+
 //Exportamos el objeto
 module.exports = controller;
