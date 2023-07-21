278c598f862eadabb4d189b01768c59dcd71e780
HectorGarcia01 <hg2779948@gmail.com>
2023-07-21 01:51:53 -0600

    Actualización de variable de entorno

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,6 @@ module.exports = {
     HOST:           process.env.HOST,
     USER:           process.env.USER,
     PASSWORD:       process.env.PASSWORD,
-    PORT:           process.env.PORT,
+    PORTMYSQL:      process.env.PORTMYSQL,
     DATABASE:       process.env.DATABASE
 }
